8ee2d30149f7e9d2cc8153c4dd1213fbe4b7e9d3
Jacob Schloss <jacob@schloss.io>
2019-05-14 18:04:22 -0700

    otghs

---
+++ include/libusb_dev_cpp/driver/stm32/stm32_h7xx_otghs.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "libusb_dev_cpp/driver/usb_driver_base.hpp"
+
+class stm32_h7xx_otghs : public usb_driver_base
+{
+public:
+
+protected:
+};
---
+++ src/driver/stm32/stm32_h7xx_otghs.cpp
@@ -0,0 +1,1 @@
+#include "libusb_dev_cpp/driver/stm32/stm32_h7xx_otghs.hpp"
