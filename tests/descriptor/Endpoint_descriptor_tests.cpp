597f2baceedd05bf4f70e9fd433a802200e10f8f
Jacob Schloss <jacob@schloss.io>
2019-05-14 19:29:10 -0700

    add tests

---
+++ tests/descriptor/Endpoint_descriptor_tests.cpp
@@ -0,0 +1,11 @@
+#include "libusb_dev_cpp/descriptor/Endpoint_descriptor.hpp"
+
+#include "gtest/gtest.h"
+
+namespace
+{
+	TEST(Endpoint_descriptor, construct)
+	{
+		Endpoint_descriptor ep_desc;
+	}
+}
