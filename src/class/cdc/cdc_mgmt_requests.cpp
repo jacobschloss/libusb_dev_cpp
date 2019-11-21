5676c4f3152989e62feb658764a9bf31558a3b87
Jacob Schloss <jacob@schloss.io>
2019-11-21 11:34:26 -0800

    disable logs

--- src/class/cdc/cdc_mgmt_requests.cpp
+++ src/class/cdc/cdc_mgmt_requests.cpp
@@ -2,10 +2,10 @@
 
 #include "common_util/Byte_util.hpp"
 
-#include "freertos_cpp_util/logging/Global_logger.hpp"
+// #include "freertos_cpp_util/logging/Global_logger.hpp"
 
-using freertos_util::logging::Global_logger;
-using freertos_util::logging::LOG_LEVEL;
+// using freertos_util::logging::Global_logger;
+// using freertos_util::logging::LOG_LEVEL;
 
 bool CDC::LINE_CODING::serialize(Line_coding_array* const out_array) const
 {
@@ -18,11 +18,11 @@ bool CDC::LINE_CODING::serialize(Buffer_adapter_tx* const out_array) const
 
 bool CDC::LINE_CODING::deserialize(const Buffer_adapter_base* buf)
 {
-	freertos_util::logging::Logger* const logger = freertos_util::logging::Global_logger::get();
+	// freertos_util::logging::Logger* const logger = freertos_util::logging::Global_logger::get();
 
 	if(buf->size() != std::tuple_size<Line_coding_array>::value)
 	{
-		logger->log(LOG_LEVEL::INFO, "CDC::LINE_CODING", "deserialize: buf wrong size");
+		// logger->log(LOG_LEVEL::INFO, "CDC::LINE_CODING", "deserialize: buf wrong size");
 		return false;
 	}
 
@@ -35,7 +35,7 @@ bool CDC::LINE_CODING::deserialize(const Buffer_adapter_base* buf)
 
 bool CDC::LINE_CODING::deserialize(const Line_coding_array& array)
 {
-	freertos_util::logging::Logger* const logger = freertos_util::logging::Global_logger::get();
+	// freertos_util::logging::Logger* const logger = freertos_util::logging::Global_logger::get();
 
 	dwDTERRate  = Byte_util::make_u32(array[0], array[1], array[2], array[3]);
 
@@ -52,7 +52,7 @@ bool CDC::LINE_CODING::deserialize(const Line_coding_array& array)
 		}
 		default:
 		{
-			logger->log(LOG_LEVEL::INFO, "CDC::LINE_CODING", "deserialize: CHAR_FORMAT unk");
+			// logger->log(LOG_LEVEL::INFO, "CDC::LINE_CODING", "deserialize: CHAR_FORMAT unk");
 			ret = false;
 			break;
 		}
@@ -70,7 +70,7 @@ bool CDC::LINE_CODING::deserialize(const Line_coding_array& array)
 		}
 		default:
 		{
-			logger->log(LOG_LEVEL::INFO, "CDC::LINE_CODING", "deserialize: PARITY_TYPE unk");
+			// logger->log(LOG_LEVEL::INFO, "CDC::LINE_CODING", "deserialize: PARITY_TYPE unk");
 			ret = false;
 			break;	
 		}
@@ -88,7 +88,7 @@ bool CDC::LINE_CODING::deserialize(const Line_coding_array& array)
 		}
 		default:
 		{
-			logger->log(LOG_LEVEL::INFO, "CDC::LINE_CODING", "deserialize: DATA_BITS unk: %d", array[6]);
+			// logger->log(LOG_LEVEL::INFO, "CDC::LINE_CODING", "deserialize: DATA_BITS unk: %d", array[6]);
 			ret = false;
 			break;	
 		}
